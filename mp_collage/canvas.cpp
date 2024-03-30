68a70269139e24b911a1cc4a244f5a0e25c57a22
Akash Shingha <akashshingha850@gmail.com>
2024-03-30 17:38:07 +0000

    ex5

--- mp_collage/canvas.cpp
+++ mp_collage/canvas.cpp
@@ -70,16 +70,16 @@ void Canvas::Swap(CanvasItem* item1, CanvasItem* item2)
 	size_t index1 = SIZE_MAX, index2 = SIZE_MAX;
     for (size_t i = 0; i < itemcount; ++i) {
         if (items[i] == item1) {
-            index1 = i;
+            for (size_t j = 0; j < itemcount; ++j) {
+                if (items[j] == item2) {
+                    // Swap the items
+                    CanvasItem* temp = items[i];
+                    items[i] = items[j];
+                    items[j] = temp;
+                    return;
+                }
+            }
         }
-        if (items[i] == item2) {
-            index2 = i;
-        }
-    }
-    if (index1 != SIZE_MAX && index2 != SIZE_MAX) {
-        CanvasItem* temp = items[index1];
-        items[index1] = items[index2];
-        items[index2] = temp;
     }
 }
 
@@ -87,53 +87,42 @@ void Canvas::Swap(CanvasItem* item1, CanvasItem* item2)
 // Blends the pixels colors of the items with their item color
 // Draws the items in their given position and scales them accordingly
 // Blends drawn layers based on alpha channel (transparency)
+
 void Canvas::draw(PNG* canvas) const
 {
-	for(size_t i=0; i<itemcount; i++) {
+	for(size_t i = 0; i < itemcount; i++) {
 		//update this line
 		CanvasItem* item = items[i];
 		
-		for(size_t x=0, xmax = item->width(); x<xmax; x++){
-			for(size_t y=0, ymax = item->height(); y<ymax; y++){
+		Vector2 pos = item->position();
+		Vector2 sc = item->scale();
+		
+		for(size_t x = 0; x < item->width(); x++) {
+			for(size_t y = 0; y < item->height(); y++) {
 				// This gets the pixel color blended with item color
 				// Check canvasitem.cpp
-				RGBAPixel coli = item->getBlendedPixel(x,y);
-				Vector2 sc = item->scale();
+				RGBAPixel coli = item->getBlendedPixel(x, y);
+				
+				int x1 = pos.x() + x * sc.x();
+				int y1 = pos.y() + y * sc.y();
 				
 				// You should uncomment the below line and use in the section below
-				Vector2 pos = item->position();
-				
+				// Vector2 pos = item->position();
+
 				// A loop is needed in case it must be scaled up, or we end up with gaps
-				for(int xs=0; xs < std::abs((int)(sc.x()-0.001))+1; xs++){
-					for(int ys=0; ys < std::abs((int)(sc.y()-0.001))+1; ys++){
-						// Determine the position on the canvas based on 
-						// item position, scale, pixel position and adjacent pixel selection
-						// TODO right now it just loops the pixels of the item
-						// x1, y1 should be coordinates of the canvas to draw to
-						// So we should add item position and adjacent pixel selection xs, ys
-						// Multiply x and y by item scale
+
+				for(int xs = 0; xs < std::abs((int)(sc.x() - 0.001)) + 1; xs++) {
+					for(int ys = 0; ys < std::abs((int)(sc.y() - 0.001)) + 1; ys++) {
+						int adj_x = x1 + xs;
+						int adj_y = y1 + ys;
 						
-						// Modify the two lines below
-						int x1 = pos.x() + x * sc.x() + xs;
-                        int y1 = pos.y() + y * sc.y() + ys;
-						
-						// Check that it's within bounds
-						if(x1 >= 0 && x1 < (int)canvas->width() && y1 >= 0 && y1 < (int)canvas->height()) {
-							// Current color of the canvas
-							RGBAPixel* colc = (*canvas)(x1,y1);
+						if(adj_x >= 0 && adj_x < (int)canvas->width() && adj_y >= 0 && adj_y < (int)canvas->height()) {
+							RGBAPixel* colc = (*canvas)(adj_x, adj_y);
 							
-							// TODO Blend based on alpha.
-							// Multiply item color with alpha, canvas color with 255-alpha
-							// Add them and divide by 255
-							// In other words 255 = replace old pixel with new. 0 = don't draw.
-							// 1-254 = partly new pixel, partly old
-							
-							// Modify the three lines below
 							colc->red = (coli.red * coli.alpha + colc->red * (255 - coli.alpha)) / 255;
                             colc->green = (coli.green * coli.alpha + colc->green * (255 - coli.alpha)) / 255;
                             colc->blue = (coli.blue * coli.alpha + colc->blue * (255 - coli.alpha)) / 255;
 							
-							// We can keep the canvas opaque, no reason to change it
 							colc->alpha = 255;
 						}
 					}
